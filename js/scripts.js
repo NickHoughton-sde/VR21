d62b671a9498c0adf98ff5b9722e414a55347eaf
Nick Houghton <njhoughton5@gmail.com>
2022-11-30 08:51:53 -0800

    scripts

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+console.log("Is this working");
+const myName = "Nick";
+const age = 23;
+const car = "a small car";
+console.log("My name is " + myName + " I am " + age + " and I drive " +car);
+const dog = "who really knows???"
+const statement = "My name is " + myName + " I am " + age + " and I drive " +car;
+console.log(statement);
+console.log(statement + " and my dog is the type: " + dog);
+console.log ("is the dog const equal to statement const?")
+console.log('"is the dog const equal to statement const?"')
+console.log(dog === statement);
+console.log("Is 123 equal to \"123\" ?");
+console.log(123 === "123");
+const str1 = "A new Car";
+console.log(str1);
+const str2 = "a new car";
+str1.toLocaleUpperCase;
+console.log(str1.toUpperCase());
+console.log("is string 1 equal to string 2 in upper case?");
+console.log(str1.toUpperCase() === str2.toUpperCase());
+console.log(str1.toUpperCase());
+console.log(str2.toUpperCase());

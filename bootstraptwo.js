c8d464e2f6e0c331e08a5ddf13cd621fb8fd7fe6
Nick Houghton <njhoughton5@gmail.com>
2022-12-12 15:28:59 -0800

    add update functionality

--- bootstraptwo.js
+++ bootstraptwo.js
@@ -1,53 +1,97 @@
 const cardBodyForm = document.getElementById("cardBodyForm");
-const deleteForm = document.querySelector("#deleteModalForm");
+const cardUpdateBodyForm = document.getElementById("cardUpdateBodyForm");
 const addButtonMain = document.getElementById("addButtonMain");
-const addBodyForm = document.getElementById("addBodyForm");
 const addSaveBtn = document.getElementById("addSaveBtn");
+const domSubModified = document.getElementById("startCards");
+const confirmDelete = document.getElementById("confirmDelete");
+const updateForm = document.getElementById("addUpdateBtn");
 let cardIdCount = 0;
+let idCardToDelete;
+let idCardToUpdate;
+let imgTitleUp;
+let imgDescUp;
+let imgUrlUp;
+let newNewArr;
+let numIdCard;
+
 
 // Event listeners
-// addBodyForm.addEventListener("submit", handleAddFormSubmit);
 cardBodyForm.addEventListener("submit", handleCardFormSubmit);
-// deleteForm.addEventListener("submit", handleConfirmDelete);
+cardUpdateBodyForm.addEventListener("submit", handleCardUpdateFormSubmit);
 addButtonMain.addEventListener("click", handleAddNewCardBtn);
+domSubModified.addEventListener("DOMSubtreeModified", setListenerDeleteUpdateBtns);
+confirmDelete.addEventListener("click", deleteCard);
+updateForm.addEventListener("click", updateCard);
 
-// function getCardToDelete(e) {
-//   let deleteId = (e.target.id);
-//   console.log(e.target.id);
-// }
-
-// Add new card when top add button clicked
 function handleAddNewCardBtn() {
   document.getElementById("addSaveBtn").addEventListener("click", appendCard);
   cardBodyForm.addImgTitle.value = "";
   cardBodyForm.addImgDesc.value = "";
   cardBodyForm.addImgUrl.value = "";
-  //need to assign when submitted not hit add!
+  
+}
+
+// this function checks for new additions to DOM and then sets event listeners for new Delete buttons
+function setListenerDeleteUpdateBtns() {
   let dButtons = document.querySelectorAll("#deleteBtn");
-  // console.log(dButtons);
   dButtons.forEach(function (dButton) {
     dButton.addEventListener("click", function (e) {
-      let deleteId = e.target.parentElement.parentElement.id;
-      console.log(e.target.id);
+      idCardToDelete = e.target.parentElement.parentElement.parentElement.parentElement.id;
+    });
+  });
+  let uButtons = document.querySelectorAll("#updateBtn");
+  uButtons.forEach(function (uButton) {
+    uButton.addEventListener("click", function (e) {
+      idCardToUpdate = e.target.parentElement.parentElement.parentElement.parentElement.id;
+      numIdCard = getNumFromString(idCardToUpdate);
     });
   });
 }
 
+// replace card in <div> element with id:stardCards
+function updateCard() {
+  let newestCard = createUpdateCard(numIdCard);
+  let oldCard = document.getElementById(idCardToUpdate);
+  
+  console.log(newestCard);
+  console.log(oldCard);
+  oldCard.replaceWith(newestCard);
+}
+
+//add delete functionality to each confirm delete button
+function deleteCard() {
+  document.getElementById(idCardToDelete).remove();
+}
+
+
+
 function handleCardFormSubmit() {
-  imgTitle = cardBodyForm.addImgTitle.value;
-  imgDesc = cardBodyForm.addImgDesc.value;
-  imgUrl = cardBodyForm.addImgUrl.value;
+  let imgTitle = cardBodyForm.addImgTitle.value;
+  let imgDesc = cardBodyForm.addImgDesc.value;
+  let imgUrl = cardBodyForm.addImgUrl.value;
   return [imgTitle, imgDesc, imgUrl];
+}
+
+function handleCardUpdateFormSubmit() {
+  imgTitleUp = cardUpdateBodyForm.inputTitleUpdate.value;
+  imgDescUp = cardUpdateBodyForm.inputDescriptionUpdate.value;
+  imgUrlUp = cardUpdateBodyForm.inputImgFileUpdate.value;
+  return [imgTitleUp, imgDescUp, imgUrlUp];
 }
 
 function appendCard() {
   let contentCard;
   contentCard = createCard();
-  //update cardIdCount to assign new number to card id for selection purposes
+  // update cardIdCount to assign new number to card id for selection purposes
   cardIdCount++;
-  document
-    .querySelector("#startCards")
-    .insertAdjacentHTML("beforeend", contentCard);
+  const newDiv = document.createElement("div", id="newDiv");
+  document.querySelector("#startCards").insertAdjacentHTML("beforeend", contentCard);
+  
+}
+
+function getNumFromString(aString) {
+  let theNum = aString.replace( /^\D+/g, '');
+  return theNum;
 }
 
 function createCard() {
@@ -65,6 +109,7 @@ function createCard() {
                 <a href="#" class="btn btn-primary" 
                     type="button"
                     class="btn btn-primary"
+                    id="updateBtn"
                     data-bs-toggle="modal"
                     data-bs-target="#updateModal">
                     Update</a>
@@ -75,11 +120,49 @@ function createCard() {
                     data-bs-toggle="modal"
                     data-bs-target="#deleteModal">
                     Delete</a>
+                    <div class ="firstDiv"></div>
               </div>
             </p>
           </div>
         </div>
       </div>`;
-  console.log(contentCardNew);
   return contentCardNew;
 }
+
+function createUpdateCard(newCardId) {
+  let contentCardNewNew = null;
+  contentCardNewNew = `<div class="col-md-3" id="totalCard${newCardId}">
+        <div class="card" style="width: 18rem;">
+          <img src="${handleCardUpdateFormSubmit()[2]}" alt="" />
+          <div>
+            <p>
+              <div class="card-body">
+                <h5 class="card-title">${handleCardUpdateFormSubmit()[0]}</h5>
+                <p class="card-text">${handleCardUpdateFormSubmit()[1]}</p>
+                <p>
+                </p>
+                <a href="#" class="btn btn-primary" 
+                    type="button"
+                    class="btn btn-primary"
+                    id="updateBtn"
+                    data-bs-toggle="modal"
+                    data-bs-target="#updateModal">
+                    Update</a>
+                <a href="#" class="btn btn-primary"
+                  type="button"
+                    class="btn btn-primary"
+                    id="deleteBtn"
+                    data-bs-toggle="modal"
+                    data-bs-target="#deleteModal">
+                    Delete</a>
+                    <div class ="firstDiv"></div>
+              </div>
+            </p>
+          </div>
+        </div>
+      </div>`;
+  return contentCardNewNew;
+}
+
+
+
